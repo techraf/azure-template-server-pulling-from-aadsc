22b73b9c1eebfe1cd1fdfcd3bf86cca71cb3bac4
techraf <techraf@gmail.com>
2016-11-26 16:18:58 +0900

    Set execution policy to unrestricted

--- CustomScript.ps1
+++ CustomScript.ps1
@@ -1,3 +1,4 @@
+Set-ExecutionPolicy Unrestricted -Force
 $networkListManager = [Activator]::CreateInstance([Type]::GetTypeFromCLSID([Guid]"{DCB00C01-570F-4A9B-8D69-199FDBA5723B}"))
 $connections = $networkListManager.GetNetworkConnections()
 $connections | % {$_.GetNetwork().SetCategory(1)}
